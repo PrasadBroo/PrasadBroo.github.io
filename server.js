1bf373278c049989cf77a14bf106d5d752b9a94d
PrasadBroo <PrasadBroo@users.noreply.github.com>
2020-05-12 18:15:05 +0530

    added server

--- server.js
+++ server.js
@@ -6,5 +6,4 @@ const port = process.env.PORT || 3000;
 app.listen(port,() => console.log(`Example app listening at http://localhost:${port}`));
 
 app.use(express.static('Website'));
-app.get('/',express.static('Website'));
 
