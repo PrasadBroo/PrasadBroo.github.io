f32ff80611b62f1c4e8fe2a52585b8b638ecff35
PrasadBroo <PrasadBroo@users.noreply.github.com>
2020-05-18 19:12:40 +0530

    hmm

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const topBtn = document.getElementById('topbtn');
 
 burger.addEventListener('click',()=>{navLinks.classList.toggle('active');burger.classList.toggle('burger-active')});
 topBtn.addEventListener('click',topFunction);
+window.onscroll = ()=>(document.body.scrollTo >20 || document.documentElement.scrollTop > 20)?topBtn.style.display="block":topBtn.style.display="none";
 
 function topFunction() {
     document.body.scrollTop = 0;
