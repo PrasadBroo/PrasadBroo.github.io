5b896dac02e7aa9ad8c4f1f4aa894ab99090c582
Prasad Shinde <gainerinfo@gmail.com>
2025-03-30 21:34:55 +0530

    Implement responsive navigation bar and theme toggle functionality; update layout and metadata for portfolio site

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useState } from "react";
+import clsx from "clsx";
+import ToggleThemeButton from "@/components/ToggleThemeButton";
+import Link from "next/link";
+
+// Array of navigation links
+const NAVLINKS = [
+  { href: "#skills", label: "Skills" },
+  { href: "#projects", label: "Projects" },
+  { href: "#experience", label: "Experience" },
+  { href: "#contact", label: "Contact" },
+];
+
+export default function Navbar() {
+  const [navActive, setNavActive] = useState<boolean>(false);
+  const listClassNames = clsx(
+    "md:static md:flex md:items-center overflow-y-hidden  md:text-base lg:text-xl  md:overflow-clip  ease-in transition-[height]  py-2 md:py-0 md:pointer-events-auto shadow-md md:shadow-none md:justify-between md:h-auto transition text-xl absolute  left-0 right-0  top-14 border-b border-gray-700 md:border-none",
+    {
+      " h-72  pointer-events-auto": navActive,
+      " h-0  pointer-events-none border-none": !navActive,
+    }
+  );
+
+  return (
+    <nav className="p-4  sticky top-0  z-10 md:flex   md:items-center shadow-md md:justify-between border-b dark:border-gray-700 border-gray-300">
+      <Link href="/" className=" text-2xl">
+        <span className=" text-gray-500">{"<"} </span>
+        <span className=" text-purple-800 dark:text-gray-50  md:text-xl lg:text-3xl hover:text-indigo-500 mx-2 tracking-widest">
+          Prasadbro
+        </span>
+        <span className="text-gray-500"> {"/>"}</span>
+      </Link>
+      <div
+        className="burger md:hidden inline-block float-right p-2"
+        onClick={() => setNavActive(!navActive)}
+        role="button"
+      >
+        <div
+          className={
+            "h-[2px] w-6 bg-foreground mt-1 transition " +
+            (navActive ? "rotate-45 " : "rotate-0")
+          }
+        ></div>
+        <div
+          className={
+            "h-[2px] w-6 bg-foreground mt-1 transition " +
+            (navActive ? " -rotate-45 translate-y-[-5px]" : " rotate-0")
+          }
+        ></div>
+        <div
+          className={
+            "h-[2px] w-6 bg-foreground mt-1 " +
+            (navActive ? " hidden" : "block")
+          }
+        ></div>
+      </div>
+      <ul className={listClassNames}>
+        {NAVLINKS.map((link) => (
+          <a
+            href={link.href}
+            key={link.label}
+            className=" lg:mr-8 md:mr-4 ease-in  hover:bg-gray-200 dark:hover:bg-purple-800 rounded-xs dark:hover:bg-purple-primary p-3  block"
+          >
+            {link.label}
+          </a>
+        ))}
+        <div className="switch p-3 hover:bg-gray-200 dark:hover:bg-purple-primary dark:hover:bg-purple-800 rounded-xs">
+          <ToggleThemeButton />
+        </div>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/components/ToggleThemeButton.tsx
@@ -0,0 +1,41 @@
+import { useTheme } from "@/contexts/ThemeContext";
+import clsx from "clsx";
+import { FaMoon, FaSun } from "react-icons/fa6";
+
+export default function ToggleThemeButton() {
+  const { toggleDarkMode, isDarkMode } = useTheme();
+
+  return (
+    <label className="flex h-6 w-full cursor-pointer items-center">
+      <div
+        className={clsx(
+          "relative inline-block  w-12 rounded-full transition-colors duration-300",
+          isDarkMode ? "bg-blue-500" : "bg-gray-400"
+        )}
+      >
+        <div
+          className={`${
+            isDarkMode ? "translate-x-6" : "translate-x-0"
+          } bg-white w-6 h-6 relative  flex items-center justify-center  rounded-full shadow-md transform transition-transform duration-300`}
+        >
+          {!isDarkMode ? (
+            <span className="absolute text-yellow-500">
+              <FaSun />
+            </span>
+          ) : (
+            <span className="absolute text-purple-800">
+              <FaMoon />
+            </span>
+          )}
+        </div>
+
+        <input
+          type="checkbox"
+          className="opacity-0 absolute w-full h-full"
+          checked={isDarkMode}
+          onChange={toggleDarkMode}
+        />
+      </div>
+    </label>
+  );
+}
