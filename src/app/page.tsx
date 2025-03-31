582f426592fd2e5fc344d76dbb52d3577f0627de
Prasad Shinde <gainerinfo@gmail.com>
2025-03-31 22:23:19 +0530

    Add ProficiencySection to Home component for showcasing skills with animated progress bars

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import LandingSection from "@/sections/LandingSection";
 import HomePageLayout from "./HomePageLayout";
 import WhatIDoSection from "@/sections/WhatIDoSection";
+import ProficiencySection from "@/sections/ProficiencySection";
 
 export default function Home() {
   return (
     <HomePageLayout>
       <LandingSection />
       <WhatIDoSection />
+      <ProficiencySection />
     </HomePageLayout>
   );
 }
