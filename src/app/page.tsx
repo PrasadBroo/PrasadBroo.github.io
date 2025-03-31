cc546cdae004a34fd61d7458004ad09b3accbab0
Prasad Shinde <gainerinfo@gmail.com>
2025-03-31 22:58:35 +0530

    Add ProjectsSection to Home component for showcasing projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import LandingSection from "@/sections/LandingSection";
 import HomePageLayout from "./HomePageLayout";
 import WhatIDoSection from "@/sections/WhatIDoSection";
 import ProficiencySection from "@/sections/ProficiencySection";
+import ProjectsSection from "@/sections/ProjectsSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <LandingSection />
       <WhatIDoSection />
       <ProficiencySection />
+      <ProjectsSection />
     </HomePageLayout>
   );
 }
