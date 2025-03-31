81d354e589a69f6996c8b7be983c43b7db997442
Prasad Shinde <gainerinfo@gmail.com>
2025-03-31 22:10:32 +0530

    Add WhatIDoSection to Home component for showcasing skills and services

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import LandingSection from "@/sections/LandingSection";
 import HomePageLayout from "./HomePageLayout";
+import WhatIDoSection from "@/sections/WhatIDoSection";
 
 export default function Home() {
   return (
     <HomePageLayout>
       <LandingSection />
+      <WhatIDoSection />
     </HomePageLayout>
   );
 }
