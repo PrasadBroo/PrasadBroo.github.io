fe8ea0c6acfd6782c3b800015bcd0c069028f111
Prasad Shinde <gainerinfo@gmail.com>
2025-04-04 10:56:39 +0530

    Enhance RootLayout by adding scroll-smooth class to html element for improved scrolling experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,7 +61,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="dark" className=" scroll-smooth">
       <body
         className={clsx(
           "antialiased text-foreground transition-colors duration-700",
