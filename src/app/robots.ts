706120352b2f2f3e63667a1d2aff8690c4afaf4e
Prasad Shinde <gainerinfo@gmail.com>
2025-04-04 11:03:05 +0530

    Add robots.txt generation with user-agent rules and dynamic sitemap URL for SEO optimization

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { DOMAIN_NAME } from "@/constants/user";
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: `https://${DOMAIN_NAME}/sitemap.xml`,
+  };
+}
