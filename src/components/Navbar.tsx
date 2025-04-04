d18f14fffb00b76e913087c987d4778dd1b4d1f3
Prasad Shinde <gainerinfo@gmail.com>
2025-04-04 11:36:48 +0530

    Refactor Navbar component to improve styling and transition effects for better user experience

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,15 +15,15 @@ const NAVLINKS = [
 export default function Navbar() {
   const [navActive, setNavActive] = useState<boolean>(false);
   const listClassNames = clsx(
-    "md:static md:flex md:visible md:items-center overflow-y-hidden  mt-2.5 md:mt-0 md:text-base lg:text-xl  md:overflow-clip  ease-in   py-2 md:py-0 md:pointer-events-auto shadow-md md:shadow-none md:justify-between md:h-auto transition-all duration-300 md:duration-0 text-xl absolute  left-0 right-0  top-14 border-b border-gray-700 md:border-none bg-background md:bg-transparent ",
+    "md:static md:flex md:visible md:items-center overflow-y-hidden  mt-2.5 md:mt-0 md:text-base lg:text-xl  md:overflow-clip  ease-in    md:py-0 md:pointer-events-auto shadow-md md:shadow-none md:justify-between md:h-auto md:max-h-28 transition-[max-height]  text-xl absolute  left-0 right-0  top-14 border-b dark:border-gray-700 border-gray-200 md:border-none bg-inherit ",
     {
-      " h-72  pointer-events-auto visible": navActive,
-      " h-0  pointer-events-none border-none invisible": !navActive,
+      " max-h-72  pointer-events-auto py-2": navActive,
+      " max-h-0  pointer-events-none border-none p-0": !navActive,
     }
   );
 
   return (
-    <nav className="p-4  sticky bg-white dark:bg-background top-0 z-10 md:flex   md:items-center shadow-md md:justify-between border-b dark:border-gray-700 border-gray-200">
+    <nav className="p-4  sticky  bg-inherit top-0 z-10 md:flex   md:items-center shadow-md md:justify-between border-b dark:border-gray-700 border-gray-200">
       <Link href="/" className=" text-2xl">
         <span className=" text-gray-500">{"<"} </span>
         <span className=" text-purple-800 dark:text-gray-50  md:text-xl lg:text-3xl hover:text-indigo-500 mx-2 tracking-widest">
